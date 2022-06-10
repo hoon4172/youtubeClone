3365e1ae19f0a3b194ec21ca7a05857959914247
hoon4172 <gns4172@naver.com>
2022-06-10 18:48:34 +0900

    videoUpload

--- client/src/components/views/VideoUploadPage/VideoUploadPage.js
+++ client/src/components/views/VideoUploadPage/VideoUploadPage.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { Typography, Button, Form, message, Input, Icon } from "antd";
 import Dropzone from "react-dropzone";
 import Axios from "axios";
+import { useSelector } from "react-redux";
 //import { response } from "express";
 
 const { Title } = Typography;
@@ -19,7 +20,8 @@ const CategoryOptions = [
   { value: 3, label: "Pets & Animals" },
 ];
 
-function VideoUploadPage() {
+function VideoUploadPage(props) {
+  const user = useSelector((state) => state.user);
   const [VideoTitle, setVideoTitle] = useState("");
   const [Description, setDescription] = useState("");
   const [Private, setPrivate] = useState(0);
@@ -76,13 +78,39 @@ function VideoUploadPage() {
     });
   };
 
+  const onSumit = (e) => {
+    e.preventDefault();
+
+    const variable = {
+      writer: user.userData._id,
+      title: VideoTitle,
+      description: Description,
+      Privacy: Private,
+      filePath: FilePath,
+      category: Category,
+      duration: Duration,
+      thumbnail: ThumbnailPath,
+    };
+    Axios.post("/api/video/uploadVideo", variable).then((response) => {
+      if (response.data.success) {
+        message.success("성공적으로 업로드를 했습니다.");
+
+        setTimeout(() => {
+          props.history.push("/");
+        }, 3000);
+      } else {
+        alert("비디오 업로드에 실패 했습니다.");
+      }
+    });
+  };
+
   return (
     <div style={{ maxWidth: "700px", margin: "2rem auto" }}>
       <div style={{ textAlign: "center", marginBottom: "2rem" }}>
         <Title level={2}>Upload Video</Title>
       </div>
 
-      <Form onSubmit>
+      <Form onSubmit={onSumit}>
         <div style={{ display: "flex", justifyContent: "space-between" }}>
           {/* Drop Zone */}
           <Dropzone onDrop={onDrop} multiple={false} maxSize={1000000000}>
@@ -147,7 +175,7 @@ function VideoUploadPage() {
         <br />
         <br />
 
-        <Button type="primary" size="large" onClick>
+        <Button type="primary" size="large" onClick={onSumit}>
           Submit
         </Button>
       </Form>
